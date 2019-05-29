e8fcbf52d0baa6f27d38742cab8cabdf61f81c69
lizKimita <kimita.wanjiku@gmail.com>
2019-05-29 11:36:56 +0300

    adding models to the admin page

--- neighbours/admin.py
+++ neighbours/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import NeighbourHood, User, Businesses
 
 # Register your models here.
+admin.site.register(NeighbourHood)
+admin.site.register(User)
+admin.site.register(Businesses)
