2e71a9cc818f8b3c65d974acb99c7fdcdece4f86
lizKimita <kimita.wanjiku@gmail.com>
2019-05-29 10:19:58 +0300

    adding initial templates base, home their views and configuring the url

--- neighbours/urls.py
+++ neighbours/urls.py
@@ -2,3 +2,8 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.home,name='home'),
+
+]
--- neighbours/views.py
+++ neighbours/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
+def home(request):
+
+    title = "Neighborhoods"
+
+    return render(request,'everything/home.html', {"title":title})
